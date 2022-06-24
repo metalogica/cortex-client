9ce7f675e9d2a8bb7fada4605ff37152c3c7c371
richard jarram <rjarram@me.com>
2022-06-24 19:49:58 -0400

    wip: tests for init cortex client

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,67 @@
+import sinon from 'sinon';
 import CortexClient from '../src/cortex-client';
 
 describe('Cortex Client', () => {
-  test('Basic configuration', () => {
-    expect(CortexClient).toBeTruthy();
+  // import WebSocketServerMock from 'mock-server';
+  let mockCortexWebsocketServer;
+
+  describe('Data streaming', () => {
+    describe('Step 1/3: Requesting a Cortex Token', () => {
+      const config = {
+        clientId: 'id',
+        clientSecret: 'secret',
+        headset: 'EPOCX-E202014A',
+      };
+
+      beforeEach(() => {
+        // TODO: mock ws:// server
+        sinon
+          .stub(mockCortexWebsocketServer, 'onMessage')
+          .returns({cortexToken: 'some-token'});
+      });
+
+      test('it should request a session token', async () => {
+        const client = CortexClient(config);
+
+        await client._getCortexToken();
+
+        expect(client.cortexToken).toBe('some-token');
+      });
+
+      test('it should throw an error if the right config is not provided', () => {
+        const client = CortexClient();
+
+        expect(client._getCortexToken()).toThrow();
+      });
+    });
+
+    test('it should create a new session', () => {
+      const client = CortexClient(config);
+      client.cortexToken = 'eySomeToken=';
+
+      expect(client._getSessionId()).toBe('session-id-123');
+    });
+
+    test('it should subscribe to a mental command stream', () => {
+      const client = CortexClient(config);
+      client.cortexToken = 'some token';
+      client.sessionId = 'session-id-123';
+
+      expect(client.stream()).toBe(true);
+    });
+  });
+
+  describe('DOM manipulation', () => {
+    const config = {
+      ui: true,
+    };
+
+    test('it should render in the DOM when specified', () => {
+      CortexClient(config);
+
+      const userInterface;
+
+      expect(userInterface).toBeTruthy();
+    });
   });
 });
--- src/cortex-client.ts
+++ src/cortex-client.ts
@@ -1,6 +1,63 @@
 class CortexClient {
   constructor(options: CortexClient) {
     console.log('cortex client class fired', options);
+
+    this.clientId = options.clientId;
+    this.clientSecret = options.clientSecret;
+
+    this.headset = options.headset;
+
+    this.ws = new WebSocket(options.url);
+  }
+
+  async stream() {
+    const cortexToken = await this._getCortexToken();
+    const sessionId = await this._getSessionId({cortexToken});
+
+    const message = {
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'subscribe',
+      params: {
+        cortexToken,
+        sessionId,
+        streams: ['com'],
+      },
+    };
+
+    await this._send({message});
+  }
+
+  async _send({message}) {
+    await this.ws.send(JSON.stringify(message));
+  }
+
+  async _getCortexToken() {
+    const message = {
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'authorize',
+      params: {
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        debit: 1,
+      },
+    };
+  }
+
+  async _getSessionId({cortexToken}) {
+    const message = {
+      id: 1,
+      jsonrpc: '2.0',
+      method: 'createSession',
+      params: {
+        cortexToken,
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        headset: this.headset,
+        status: 'active',
+      },
+    };
   }
 }
 
